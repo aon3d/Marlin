28f9402e65d32e58beae81d01b4f5dab62d6c9bf
Kevin Han <kevin@aon3d.com>
2018-03-01 12:04:09 -0500

    Z max comment removed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -834,7 +834,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 620 // investigate max travel distance and confirm
+#define Z_MAX_POS 620 
 
 /**
  * Software Endstops
