882f92f2f5dcd74926c1acad99e1080bf5606f7b
Kevin Han <kevin@aon3d.com>
2018-03-01 02:44:54 -0500

    Filament Sensor Logic

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -871,8 +871,8 @@
  */
 #define FILAMENT_RUNOUT_SENSOR
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
-  #define FIL_RUNOUT_INVERTING false // set to true to invert the logic of the sensor.
-  #define ENDSTOPPULLUP_FIL_RUNOUT // Uncomment to use internal pullup for filament runout pins if the sensor is defined.
+  #define FIL_RUNOUT_INVERTING true // set to true to invert the logic of the sensor.
+  //#define ENDSTOPPULLUP_FIL_RUNOUT // Uncomment to use internal pullup for filament runout pins if the sensor is defined.
   #define FILAMENT_RUNOUT_SCRIPT "G28 X\nM0" //Home X, Wait. M108 to continue.
 #endif
 
